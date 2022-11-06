d29d16af4ddcd3ac8a229cf089b08baf68e7b73f
Cyrus <cyrus.kuznetsov@gmail.com>
2022-11-06 23:18:32 +0300

    feat: add console messages in task 4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -5,12 +5,15 @@ const source = join(__dirname, 'files');
 const dist = join(__dirname, 'files-copy');
 
 (async function() {
+    console.log('Creating folder...');
     await rm(dist, { recursive: true, force: true });
     await mkdir(dist);
 
+    console.log('Copy files...');
     (await readdir(source, { encoding: "utf8", withFileTypes: true }))
         .forEach( file => {
             if (file.isFile()) 
                 copyFile(join(source, file.name), join(dist, file.name));
         });
+    console.log('Done!');
 })();
