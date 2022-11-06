f7bb2e72b8eec03f29c8467a140448c599c1555b
Cyrus <cyrus.kuznetsov@gmail.com>
2022-11-06 15:55:44 +0300

    refactor: remove redundant map and filter calls

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -7,24 +7,14 @@ const dist = join(__dirname, 'project-dist');
 
 const bundlePath = join(dist, 'bundle.css');
 
-async function main() {
+(async function() {
     let writeStream = createWriteStream(bundlePath, 'utf8');
 
-    let files = (await readdir(
-        source,
-        {
-            encoding: "utf8",
-            withFileTypes: true
-        }
-    ))
-    .filter(file => file.isFile())
-    .map(file => join(source, file.name))
-    .filter(file => extname(file) === '.css')
+    (await readdir( source, { encoding: "utf8", withFileTypes: true}))
+    .filter(file => file.isFile() && extname(file.name) === '.css')
     .forEach(file => {
-        let fileRead = createReadStream(file, 'utf8');
-        fileRead.on('end', () => console.log(`Done! (${file})`));
+        let fileRead = createReadStream(join(source, file.name), 'utf8');
+        fileRead.on('end', () => console.log(`Done! (${file.name})`));
         fileRead.pipe(writeStream, { end: false });
     });
-}
-
-main();
+})();
