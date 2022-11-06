6ae5fa94bc6ad39522cd516374f9063cc88bbb19
Cyrus <cyrus.kuznetsov@gmail.com>
2022-11-06 22:18:04 +0300

    refactor: refactor task 3

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,37 +1,28 @@
-const { promises: fs } = require("fs");
-const { extname, join } = require('path');
+const { stat, readdir } = require("fs/promises");
+const { join,  parse } = require('path');
 
 const filePath = join(__dirname, 'secret-folder');
 
-async function getFileData(fileDirent) {
-    let path = join(filePath, fileDirent.name);
-    return {
-        name: fileDirent.name,
-        ext: extname(path).substring(1),
-        size: (await fs.stat(path)).size / 1024,
-    }
-}
-
-async function getFilesData(files) {
-    files = files.filter((file) => file.isFile());
-    let newFiles = [];
-    for (let file of files) newFiles.push(await getFileData(file));
-    return newFiles;
-}
-
-async function main() {
-    let files = await fs.readdir(
+(async function() {
+    let files = await readdir(
         filePath,
         {
             encoding: "utf8",
             withFileTypes: true
         }
     );
-
-    let data = await getFilesData(files);
+    let data = [];
+    for (let file of files) {
+        if (file.isFile()) {
+            let parsed = parse(file.name);
+            data.push({
+                name: parsed.name,
+                ext: parsed.ext.substring(1),
+                size: (await stat(join(filePath, file.name))).size / 1024,
+            })
+        }
+    }
     console.log('List of files: ');
     for (const file of data)
         console.log(`${file.name} - ${file.ext} - ${file.size.toFixed(2)}kb`);
-}
-
-main();
+})();
