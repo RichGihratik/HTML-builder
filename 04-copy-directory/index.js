6f12a1ac7a32969f2c1705b8f94c1dcaa14337a7
Cyrus <cyrus.kuznetsov@gmail.com>
2022-11-06 23:10:51 +0300

    refactor: remove files variable, remove main

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -2,22 +2,13 @@ const { mkdir, readdir, copyFile, rm } = require('node:fs/promises');
 const { join } = require('path');
 
 const source = join(__dirname, 'files');
-const dist = join(__dirname, 'files-copy')
+const dist = join(__dirname, 'files-copy');
 
-async function main() {
+(async function() {
     await rm(dist, { recursive: true, force: true });
     await mkdir(dist);
 
-    let files = (await readdir(
-        source,
-        {
-            encoding: "utf8",
-            withFileTypes: true
-        }
-    ))
-    .filter(file => file.isFile())
-    .forEach((file) => copyFile(join(source, file.name), join(dist, file.name)));
-}
-
-main();
-
+    (await readdir( source, { encoding: "utf8", withFileTypes: true }))
+        .filter(file => file.isFile())
+        .forEach((file) => copyFile(join(source, file.name), join(dist, file.name)));
+})();
