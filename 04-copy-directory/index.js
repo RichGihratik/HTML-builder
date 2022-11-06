ef2178a1b728ad9a74f7165adf7c77819f16afbe
Cyrus <cyrus.kuznetsov@gmail.com>
2022-11-06 23:13:44 +0300

    refactor: replace filter call

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -8,7 +8,9 @@ const dist = join(__dirname, 'files-copy');
     await rm(dist, { recursive: true, force: true });
     await mkdir(dist);
 
-    (await readdir( source, { encoding: "utf8", withFileTypes: true }))
-        .filter(file => file.isFile())
-        .forEach((file) => copyFile(join(source, file.name), join(dist, file.name)));
+    (await readdir(source, { encoding: "utf8", withFileTypes: true }))
+        .forEach( file => {
+            if (file.isFile()) 
+                copyFile(join(source, file.name), join(dist, file.name));
+        });
 })();
